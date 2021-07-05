0b5526e555f64bca63e5203d4eb08e5b17d329ff
paojar <6210110356@psu.ac.th>
2021-07-05 11:45:50 +0700

    help

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import CharacterCard from './CharacterCard';
+import WordCard from './WordCard';
 import React from 'react';
 import './App.css';
 const word = "Hello";
 function App() {
- return (
- <div>
- {
- Array.from(word).map((c, i) => <CharacterCard value={c} key={i}/>)
- }
- </div>
- );
+  return (
+    <div>
+    <WordCard value="hello"/>
+    </div>
+    );
 }
 export default App
